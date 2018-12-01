b123040e9fe50a70f41790f70296977326495da3
mfaizalsg <38887534+mfaizalsg@users.noreply.github.com>
2018-12-01 10:27:30 +0800

    Update faizal.ps1

--- faizal.ps1
+++ faizal.ps1
@@ -1,1 +1,11 @@
-Get-Service | Where-Object {$_.Status –eq “Running”}
+Write-Host –NoNewLine "Counting from 1 to 9 (in seconds):  "
+
+foreach($element in 1..9){
+
+  Write-Host –NoNewLine  "${element} "
+
+  Start-Sleep –Seconds 1
+
+}
+
+Write-Host ""
